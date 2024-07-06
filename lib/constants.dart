270e67268aed12a45d5c7388503a10d5e1b192ae
TheRealKwabena <gusasarkw@student.gu.se>
2024-07-06 23:53:01 +0200

    testing for user account

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,6 @@ class Constants {
   static const Color darkShadeBlue = Color(0xFF17181C);
   static const Color mainBackgroundColor = Color(0xFF1A1C23);
   static const Color greyColor = Color(0xFFD9D9D9);
+
   static const Color appBarBackgroundColor = Color.fromRGBO(13, 11, 11, 1);
 }
